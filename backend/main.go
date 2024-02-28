8ecafe403a01a5640300d3f32b634e03fce87f3f
Mohammad Ashraf <mohammadashraf2001@gmail.com>
2024-02-28 11:25:08 +0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+// in this block we define our upgrader, which will be used to upgrade our http connection to a websocket connection
+var upgrader = websocket.Upgrader{
+	// Allow all origins
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// in this block, we define our reader, which will listen for messages on our websocket connection
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+		//fmt.Println("Received: %s%d", string(p), messageType)
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	// upgrade our raw http connection to a websocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// listen for messages
+	reader(ws)
+}
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Simple Server")
 	})
+	// define our route for our websocket connection
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
