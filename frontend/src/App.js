87657a13be9b050c14345a8d9e982bc2bf095688
Mohammad Ashraf <mohammadashraf2001@gmail.com>
2024-02-29 09:58:10 +0300

    Added Header and chat history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
+import Header from "./components/Header/Header";
 
 function App() {
-  connect();
+  const [chatHistory, setChatHistory] = useState([]);
+
+  useEffect(() => {
+    connect((msgEvent) => {
+      const msg = msgEvent.data;
+      console.log("New Message:", msg);
+      setChatHistory((prevChatHistory) => [...prevChatHistory, msg]);
+    });
+  }, []);
+
   const send = () => {
     console.log("hello");
     sendMsg("hello");
@@ -10,8 +21,19 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <button onClick={send}>Hit</button>
+
+        {/* Display chat history */}
+        <div>
+          <h2>Chat History</h2>
+          <ul>
+            {chatHistory.map((message, index) => (
+              <li key={index}>{message}</li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
